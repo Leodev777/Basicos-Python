ca1924e89379408874b0c8f3bc8dbe522f340a61
Leonardogit <leonardoiglesias74@gmail.com>
2025-07-27 13:16:27 -0300

    T2-C2

--- Tema 2 Test Condicionales, if-statements y switch-case/Clase 2 Práctica 2a Test Condicionales, if-statements y switch-case/Código division.py
+++ Tema 2 Test Condicionales, if-statements y switch-case/Clase 2 Práctica 2a Test Condicionales, if-statements y switch-case/Código division.py
@@ -10,3 +10,14 @@ else:
     print("El resultado de dividir", n, "entre", m, "es", division)
 # Si no es cero realizamos la division
 # Si es cero imprimimos un error
+
+## ----------------------------------------------------- ##
+
+dividendo = float(input("Cargar dividendo"))
+divisor = float(input("Cargar divisor"))
+
+if dividendo == 0:
+    print("ERROR: No se puede dividir")
+else:
+    division = dividendo/divisor
+    print("El resultado de dividir", dividendo, "entre ", divisor, "es ", division)    
