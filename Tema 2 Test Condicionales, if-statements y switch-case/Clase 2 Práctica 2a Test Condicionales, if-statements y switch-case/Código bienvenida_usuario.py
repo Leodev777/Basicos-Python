02ddac76f8e4530ccd9f61aeed39bd9d297a709a
Leonardogit <leonardoiglesias74@gmail.com>
2025-07-27 11:41:15 -0300

    T2-C2

--- Tema 2 Test Condicionales, if-statements y switch-case/Clase 2 Práctica 2a Test Condicionales, if-statements y switch-case/Código bienvenida_usuario.py
+++ Tema 2 Test Condicionales, if-statements y switch-case/Clase 2 Práctica 2a Test Condicionales, if-statements y switch-case/Código bienvenida_usuario.py
@@ -18,3 +18,26 @@ if nombre.lower() == usuario_1 or nombre.lower() == usuario_2 or nombre.lower() 
 else:
     print("¡Bienvenido usuario invitado!")
 
+
+# Pedir nombre
+
+nombre = input('Carga tu nombre!!!!!!!!')
+
+# variables con nombres de los usuarios
+
+usuario_4 = "Chiche"
+usuario_5 = "Cachito"
+usuario_6 = "Raulio"
+
+# comparamos nombres ingreados con nombres de variables
+
+if nombre.lower == usuario_4 or nombre.lower == usuario_5 or nombre.lower == usuario_6:
+    print('Bienvenido', nombre.title, "!")
+else:
+    print('Bienvenido!')
+
+
+
+# Si coincide el nombre damos bienvenida al usuario
+
+# Si no damos la generica
--- Tema 2 Test Condicionales, if-statements y switch-case/Clase 2 Práctica 2a Test Condicionales, if-statements y switch-case/Código constraseña_segura.py
+++ Tema 2 Test Condicionales, if-statements y switch-case/Clase 2 Práctica 2a Test Condicionales, if-statements y switch-case/Código constraseña_segura.py
@@ -1,7 +1,7 @@
-# --- Pedir contraseña al usuario
+# Pedir contraseña al usuario
 password = input("Introduce la contraseña ")
 
-# --- Comprobar si la contraseña es segura
+# Comprobar si la contraseña es segura
 # Si cumple los requisistos diremos que es segura
 # Debe tener una vocal (a,e,o)
 # Debe tener un simbolo especial (*,#)
@@ -10,3 +10,17 @@ if ("a" in password or "e" in password or "o" in password) and ("*" in password 
 else:
     print("La constraseña no es segura")
 # Si no los cumple diremos que es insegura
+
+## --------------------------------------------------------
+
+password = input("Introducir la contraseña, que contenga volares y simobolos * # ")
+
+# comprobar contraseña segurda
+# si coumple los requisitros diremos que es segura
+# debe tener una vocal
+# debe tener un simbolo especial
+
+if ("a" in password or "e" in password or "i" in password or "o" in password or "u" in password) and ("*" in password or "#" in password):
+     print("Contraseña segura")
+else:
+     print("Cargar contraseña con volares y simobolos")     
