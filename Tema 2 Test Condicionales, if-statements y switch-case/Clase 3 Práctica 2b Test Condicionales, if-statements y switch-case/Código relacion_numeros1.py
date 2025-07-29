21f17ad40dfe434be25aa82a574a960c4d654f77
Leonardogit <leonardoiglesias74@gmail.com>
2025-07-29 15:03:23 -0300

    T2-C3

--- Tema 2 Test Condicionales, if-statements y switch-case/Clase 3 Práctica 2b Test Condicionales, if-statements y switch-case/Código relacion_numeros1.py
+++ Tema 2 Test Condicionales, if-statements y switch-case/Clase 3 Práctica 2b Test Condicionales, if-statements y switch-case/Código relacion_numeros1.py
@@ -1,18 +1,39 @@
-# --- Pedir tres numeros
-x = float(input("Introduce el primer numero: "))
-y = float(input("Introduce el segundo numero: "))
-z = float(input("Introduce el tercer numero: "))
+# Pedir tres numeros
+#x = float(input("Introduce el primer numero: "))
+#y = float(input("Introduce el segundo numero: "))
+#z = float(input("Introduce el tercer numero: "))
 
-# --- Comprobar si alguno de ellos es la suma de los otros dos
+# Comprobar si alguno de ellos es la suma de los otros dos
 # Si es cierto imprimimos True
-if (x==y+z):
-    print("El primero es la suma de los otros dos")
-elif (y==x+z):
-    print("El segundo es la suma de los otros dos")
-elif (z==x+y):
-    print("El tercero es la suma de los otros dos")
+#if (x==y+z):
+#    print("El primero es la suma de los otros dos")
+#elif (y==x+z):
+#    print("El segundo es la suma de los otros dos")
+#elif (z==x+y):
+#    print("El tercero es la suma de los otros dos")
 
 # Si es falso imprimir False
-else:
-    print("Ninguno es la suma de los otros dos numeros")
+#else:
+#    print("Ninguno es la suma de los otros dos numeros")
 
+
+
+## --------------------------------------------------------------------------------------- ##
+
+
+# Cargamos los numeros
+
+A = float(input("Cargar el primer numero "))
+B = float(input("Carga el segundo numero "))
+C = float(input("Carga el tercer numero "))
+
+# Hacemos las comparaciones
+
+if (A == B+C):
+    print("El primero es la suma de los otros dos")
+elif (B == A+C):
+    print("El segundo es la suma de los otros 2")
+elif (C == B+A):
+    print("El tercero es la suma de los otros 2")
+else:
+    print("Ninguna de los anteriores")        
