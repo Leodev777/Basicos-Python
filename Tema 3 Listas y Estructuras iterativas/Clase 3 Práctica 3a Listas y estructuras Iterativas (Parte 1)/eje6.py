24e6eeda4a5faa14396f4f5f5c994ffcb5305030
Leonardogit <leonardoiglesias74@gmail.com>
2025-08-05 18:18:21 -0300

    T3-C3

--- Tema 3 Listas y Estructuras iterativas/Clase 3 Práctica 3a Listas y estructuras Iterativas (Parte 1)/eje6.py
+++ Tema 3 Listas y Estructuras iterativas/Clase 3 Práctica 3a Listas y estructuras Iterativas (Parte 1)/eje6.py
@@ -6,12 +6,15 @@ no tiene un divisor positivo y entero que no sea 1 o si mismo.
 '''
 num = 0
 # bucle que recorre los numeros del 2 al 100
-for num in range(2, 101):
-    primo = True
+for num in range(2, 101): # 2 es el primer primo
+    # variable que indica si es primo
+    primo = True # inicializamos como primo
+    # bucle que comprueba si es primo
+    # si es divisible por algun numero entre 2 y num-1, no es primo
 
-    for i in range(2, num):
-      if num % i == 0:
-            primo = False
-    if primo:
-        print(num)     
+    for i in range(2, num): # comprobamos desde 2 hasta num-1
+      if num % i == 0: # si es divisible % es el resto de la division
+            primo = False # no es primo
+    if primo: # si es primo, lo imprimimos
+        print(num)     # Imprime el numero primo
 # Para dicho numero, hay alguno que sea sub divisor
