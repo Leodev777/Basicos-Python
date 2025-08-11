3025a1329c5ef8bc59a6e0aa826e023de73d15d8
Leonardogit <leonardoiglesias74@gmail.com>
2025-08-11 12:57:59 -0300

    T3-C5

--- Tema 3 Listas y Estructuras iterativas/Clase 5 Práctica 3b Listas y Estructuras Iterativas/Código numero_primos_2.py
+++ Tema 3 Listas y Estructuras iterativas/Clase 5 Práctica 3b Listas y Estructuras Iterativas/Código numero_primos_2.py
@@ -1,5 +1,5 @@
 '''
-Dado una lista de números enteros, 
+Dado una lista de números enteros,
 escribe un script en Python que 
 devuelva una nueva lista con los 
 números primos de la lista original. 
@@ -9,4 +9,28 @@ la suma de los números primos encontrados
 '''
 
 # Lista de números enteros
+# Lista vacia para primos
+# Numeros total de numeros primos
+# Suma de los numeros primos
+
 numeros = [10, 15, 23, 42, 7, 19, 4, 29, 30]
+primos = []
+total_primos = 0
+suma_primos = 0
+
+# Bucle para recorrer la lista de números
+for numero in numeros:
+    primo = True
+    # Comprobar si el número es primo
+    for i in range(2, numero):
+        if numero % i == 0:
+            primo = False
+        # Si es primo, añadir a la lista de primos
+    if primo == True:
+        primos.append(numero)
+        print(f"{numero} es primo")
+        print(f"Total de primos: {len(primos)}")
+
+total_primos = total_primos +1
+suma_primos = suma_primos + numero
+print(f"Suma de primos: {suma_primos}")
