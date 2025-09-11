92fa3e56f4820b42da804488983ba1e778dffd73
Leonardogit <leonardoiglesias74@gmail.com>
2025-09-11 07:37:23 -0300

    T3 C10

--- Tema 3 Listas y Estructuras iterativas/Clase 10 Práctica Bonus Listas y Estructuras Iterativas/lista_1.py
+++ Tema 3 Listas y Estructuras iterativas/Clase 10 Práctica Bonus Listas y Estructuras Iterativas/lista_1.py
@@ -25,5 +25,9 @@ base_datos.append(datos_alumno)
 for alumno in base_datos:
     dni = alumno[2] # extraemos el dni del alumno
     # calculamos la media del alumno
+    suma_notas = 0 # variable para almacenar la suma de las notas
     for i in range(5,len(alumno)): # recorremos todas las notas del alumno
-        suma_notas = 0
+        print(alumno[i])
+        suma_notas = suma_notas + float(alumno[i]) # sumamos la nota a la variable suma_notas
+    media = suma_notas / (len(alumno) - 5) # calculamos la media dividiendo la suma de las notas entre el numero de notas
+    print(f"El alumno con DNI {dni} tiene una nota media de {media:.2f}") # imprimimos el dni y la nota media del alumno
