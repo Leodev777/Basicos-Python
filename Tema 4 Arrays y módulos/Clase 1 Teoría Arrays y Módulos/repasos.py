3ff7d739ac34af2bf39d71c6247a16308335ca2e
Leonardogit <leonardoiglesias74@gmail.com>
2025-09-12 12:17:29 -0300

    T4 C1

--- Tema 4 Arrays y módulos/Clase 1 Teoría Arrays y Módulos/repasos.py
+++ Tema 4 Arrays y módulos/Clase 1 Teoría Arrays y Módulos/repasos.py
@@ -47,8 +47,70 @@ print(area)
 '''
 Concepto de paqueres y librerias:
 
-Un paquete es una carpeta que contiene varios modulos,
-podemos,
+Un paquete/package es un directorio o carpeta con una coleccion
+de modulos, podemos tener paquetes y subpaquetes con distintos
+modulos en cada uno de ellos
 
+my_paquete/
+    __init__.py # archivo que indica que es un paquete
+    trainig.py # archivo con codigo python
+    ...
+    ...
+    submission.py # archivo con codigo python 
+    ...
+    ...
+    subpaquete/
+        __init__.py # archivo que indica que es un paquete
+        metrics.py # archivo con codigo python  
+       
+        ... 
+
+        
+Tenemos los paquetes y subpaquetes, cada uno con sus modulos
+y cada modulo con su codigo python, podemos importar
+cualquier modulo desde cualquier otro modulo.
+
+
+LIBRERIAS: Es un termino general para referirse a
+una pieza de codigo reutilizable. 
+Normalmente una libreria a contiene una coleccion de
+paquetes y modulos.
+
+EJ: Numpy, Pandas, Matplotlib, pytorch, ...
+
+"Libreria": es una coleccion de paquetes
+"Un paquete": es una colecicon de modulos
+
+Ahora vamos a los arrays:
+
+Array: Son contenedores que son capaces de guardar mas de un objeto al 
+mismo tiempo. COleccion ordenada de elementos.
+
+Diferencia entre listas y arrays:
+
+Lista Es una estructura in-built / Array es una estructura que es necesario importar usando array o numpy
+Lista Se crea usando [] / Array se crea usando array() o numpy.array()
+Lista Puede contener elementos de distintos tipos / Array no puede contener elementos de distintos tipos
+Lista El anidamiento de estructuras de distinta dimension es posible / Array Solo se pueden anidar arrays con mismo tamaño
+-----> Lista No se puede aplicar operaciones aritmeticas / Array se pueden aplicar operaciones aritmeticas 
+Lista Consume mas memoria / Array consume menos memoria
+Lista mayor flexibilidad a la hora de modifcar datos / Array menos flexibilidad a la hora de modificar datos
 
 '''
+
+# Ejemplo lista (no se puede aplicar operaciones aritmeticas)
+mi_lista = [1, 2, 3, 4, 5]
+nueva_lista = mi_lista + 2 # Error
+print(nueva_lista)
+# para poder realizar operaciones aritmeticas dentro de una lista debemos
+# realizar un bucle for
+
+mi_lista = [1, 2, 3, 4, 5]
+nueva_lista = []
+
+for i in mi_lista:
+    nueva_lista.append(mi_lista(i) * 3)
+print(nueva_lista)
+
+# Ejemplo array (se pueden aplicar operaciones aritmeticas)
+
