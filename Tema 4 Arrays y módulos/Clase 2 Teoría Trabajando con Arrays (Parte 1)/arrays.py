d73e6956a7fbebbb9b08a2efcda13e09c3931529
Leonardogit <leonardoiglesias74@gmail.com>
2025-09-14 15:34:03 -0300

    T4 C2

---
+++ Tema 4 Arrays y módulos/Clase 2 Teoría Trabajando con Arrays (Parte 1)/arrays.py
@@ -0,0 +1,16 @@
+# El sentido principarl de convertir listas en arrays es por
+# una cuestion de eficiencia y velocidad
+
+# sabemos que en las listas podemos tener diferentes tipos de datos
+# booleanos, entetos, strings, etc vamos a dar un ejemplo
+
+# booleano! verdadero o falso / 1 o 0 / solo tengo 2 opciones
+# pero en string podemos tener 
+# 26 minuculas
+# 26 mayusculas
+# 10 signos de puntuacion
+# entonces para 1 solo caracter tenemos +70 opciones
+# esto hace que las listas sean mas lentas ya que tienen
+# que reservar mas espacio en memoria para cada elemnto
+# en cambio los arrays son mas eficientes ya que solo
+# pueden tener un tipo de dato
