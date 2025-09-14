0fcb1515a86d2b9f881297afd871bfdfd1f963af
Leonardogit <leonardoiglesias74@gmail.com>
2025-09-14 15:44:24 -0300

    T4 C2

--- Tema 4 Arrays y módulos/Clase 2 Teoría Trabajando con Arrays (Parte 1)/arrays.py
+++ Tema 4 Arrays y módulos/Clase 2 Teoría Trabajando con Arrays (Parte 1)/arrays.py
@@ -14,3 +14,30 @@
 # que reservar mas espacio en memoria para cada elemnto
 # en cambio los arrays son mas eficientes ya que solo
 # pueden tener un tipo de dato
+
+import numpy as np
+# de lista a arrays
+
+lista_a_array = np.array([1,2,3,4,5])
+print(type(lista_a_array[0]))
+# Esto regresa que es un entero de 64 bits
+
+# Necestamos 64 bits para representar los numeros 1 2 y 3?
+
+# 1 es 01
+# 2 es 10
+# 3 es 11
+# en este caso el maximo de bits que necesitamos es 2
+# y no 64.
+
+# para poder consumir menos memoria podemos pasar de 64 a 8 bites, como? 
+
+lista_a_array = np.array([1,2,3,4,5], dtype=np.int8) # int8 es un entero de 8 bits
+print(type(lista_a_array[0])) # Esto regresa que es un entero de 8 bits
+
+# esta es una de las cosas por las cueles los arrays son tan importantes! y asi podemos optimizar nuestro cdigo
+
+# array bisimensional
+
+lista_a_array_bi = np.array([[1,2,3],[4,5,6],[7,8,9]], dtype=np.int8)
+print(lista_a_array_bi)
