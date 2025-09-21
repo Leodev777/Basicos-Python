5daeda0949eed9b7320cb40fc4ec82256daef38d
Leonardogit <leonardoiglesias74@gmail.com>
2025-09-21 12:20:00 -0300

    T4 C5

--- Tema 4 Arrays y módulos/Clase 5 Práctica 4a Trabajando con Arrays (Parte 2)/1d-parte2.py
+++ Tema 4 Arrays y módulos/Clase 5 Práctica 4a Trabajando con Arrays (Parte 2)/1d-parte2.py
@@ -1,26 +1,57 @@
 import numpy as np
 
-array_randon = np.random.randint(1,101, size=15) # generamos numeros aleatorios entre 1 y 100 con random.randint y size para crear el array unidemensinal de 15 elementos
+# ------------------- CREACIÓN DE ARRAYS -------------------
 
+# Array de 15 enteros aleatorios entre 1 y 100
+array_enteros = np.random.randint(1, 101, size=15)
+
+# Array de 15 decimales aleatorios entre 0.0 y 1.0
 array_decimales = np.random.random(15)
 
-print("Array random ", array_randon)
-print("Array decimales ", array_decimales)
+print("Array de enteros   :", array_enteros)
+print("Array de decimales :", array_decimales)
 
-# Suma usando operador
+# ------------------- OPERACIONES -------------------
 
-suma_operador = array_randon - array_decimales
-print("Suma usando operador ", suma_operador)
+# 1. Suma usando el operador +
+suma_operador = array_enteros + array_decimales
+print("\nSuma con operador + :", suma_operador)
 
+# 2. Suma usando la función de NumPy
+suma_numpy = np.add(array_enteros, array_decimales)
+print("Suma con np.add      :", suma_numpy)
 
-# -------------------------- ADD SUMA
+# 3. Resta usando el operador -
+resta_operador = array_enteros - array_decimales
+print("\nResta con operador - :", resta_operador)
 
-suma_de_operadores = np.add(array_randon, array_decimales)
-print("La suma es: ", suma_de_operadores)
+# 4. Resta usando la función de NumPy
+resta_numpy = np.subtract(array_enteros, array_decimales)
+print("Resta con np.subtract:", resta_numpy)
 
-# -------------------- 
+import numpy as np
 
-resta_numpy = np.subtract(array_randon, array_decimales)
-print("La resta es ", resta_numpy)
+# ------------ MULTIPLICACIÓN CON NUMPY ------------
 
+# Dos valores simples
+coso1 = 1
+cosito2 = 3
 
+# Multiplicación con np.multiply
+multi_numpy = np.multiply(coso1, cosito2)
+print("Multiplicación con np.multiply:", multi_numpy)
+
+# ------------ ARRAY ALEATORIO Y VALOR MÁXIMO ------------
+
+# Generamos un array con 15 números enteros entre 1 y 100
+array_random = np.random.randint(1, 101, size=15)
+print("\nArray random:", array_random)
+
+# Buscamos el valor más alto en ese array
+maximo_valor = np.max(array_random)
+print("El valor máximo es:", maximo_valor)
+
+# Media de valores con mean()
+
+media_ramdom = np.mean(array_random)
+print("Valor medio!", media_ramdom)
