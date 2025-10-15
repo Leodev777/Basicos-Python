84be1fcf490fe73c1a7eff71ae03a7ded48b8e61
Leonardogit <leonardoiglesias74@gmail.com>
2025-10-15 10:24:54 -0300

    T5 C1

---
+++ Tema 5 Tuplas y Sets/Clase 1 Teoría Tuplas y Sets (Parte 1)/notas.py
@@ -0,0 +1,33 @@
+# Tuplas: Que son y como funcionan
+
+# Estructura de datos:
+
+# 1. Listas: (in-built: vienen dentro de python) son mutables, es decir, se pueden modificar despues de su creacion
+# 2. Arrays: (se importan modulos) son similares a las listas, pero solo pueden contener el mismo tipo de dato
+# 3. Tuplas: son inmutables, es decir, no se pueden modificar despues de su creacion
+
+# Cuales son las estructuras de datos in built de python?
+# 1. Listas
+# 2. Tuplas
+# 3. Diccionarios
+# 4. Sets
+
+# TUPLAS: LISTAS INMUTABLES
+# 1. No permiten añadir, eliminar o mover elementos ( append, remove)
+# 2. Permiten extraer porciones pero eso da como resultado una nueva tupla
+# 3. Permite comprobar si un elemento se encuentra en la tupla
+
+# Sn mas rapidas que las listas
+# Ocupan menos espacio (Mayor optimizacion de memoria)
+# Pueden usarse como llaves de un diccionario
+
+# Sintaxis de una Tupla!
+
+mi_tupla_1 = ("fruta", 45, True)
+print(type(mi_tupla_1))
+
+# Accerder a los emelementos de una tupla
+
+mi_tupla_1 = ("verdura", 45, True)
+print(mi_tupla_1[0])
+
