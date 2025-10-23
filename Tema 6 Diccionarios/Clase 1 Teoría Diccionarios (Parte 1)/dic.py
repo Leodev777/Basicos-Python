7fd67b450dead31790eab2eef16068885af5c5d1
Leonardogit <leonardoiglesias74@gmail.com>
2025-10-23 08:47:47 -0300

    T6 C1

--- Tema 6 Diccionarios/Clase 1 Teoría Diccionarios (Parte 1)/dic.py
+++ Tema 6 Diccionarios/Clase 1 Teoría Diccionarios (Parte 1)/dic.py
@@ -112,4 +112,123 @@ persona = {
 edad = persona['edad']
 print(edad) # salida 23
 
+# Metodos especificos para diccionarios
+# Luego tenemos metodos que no son especificos pero los podemos usar en los diccionarios
 
+# Keys():
+
+diccionario_x = {'pera': 1, 'manzana': 2, 'toronja': 3}
+print(diccionario_x.keys()) # salida dict_keys(['pera', 'manzana', 'toronja'])
+
+# Get():
+
+diccionario_xx = {'pera': 1, 'manzana': 2, 'toronja': 3}
+print(diccionario_xx.get('manzana')) # Salida 2
+print(diccionario_xx.get('wiki')) # Salida none
+
+# si buscamos una clave que no existe con los corchetes nos
+# dara un error keyerror
+
+# diccionario_xx = {'pera': 1, 'manzana': 2, 'toronja': 3}
+# print(diccionario_xx['tomate']) # salida keyerror
+
+# El metodo get tiene caracteristicas interesantes
+# agregmoas el .get y la clave que me ieteresa y nos retorna el valor
+# pero si introducimos una clave que no existe nos 
+# retorna none
+# pero podemos agregar un segundo parametro al get
+# que seria el valor por defecto en caso de que la clave no
+# exista
+# EJ: 
+
+diccionario_xx = {'pera': 1, 'manzana': 2, 'toronja': 3}
+print(diccionario_xx.get('manzana')) # Salida 2
+print(diccionario_xx.get('wiki',0)) # salida 0
+
+# devuelve el valor de una clave en un diccionario.
+# Si la clave no existe, devuelve un valor predeterminado
+# en este caso 0 en lugar de generar un error.
+
+
+# Ahora metodos que ya conocemos pero aplicados a los diccionarios
+# Pop():
+# Elimina y devuelve el valor de una clave en un diccionario
+
+diccionarioo = {'pera': 1, 'manzana': 2, 'toronja': 3}
+valorrr = diccionarioo.pop('manzana')
+print(diccionarioo)
+print(valor)
+
+# pop no solo elimina sino que tambien guarda
+
+
+
+# DE TUPLAS A DICCIONARIOS
+# Sintaxis:
+key1 = 'tateti'
+value1 = 0
+key2 = 'tateta'
+value2 = 0
+key3 = 'tateto'
+value3 = 0
+mis_tuplass = [(key1, value1), (key2, value2), (key3, value3)]
+my_dicc = dict(mis_tuplass) # Lista de tuplas
+
+# Ejemplo:
+
+eventos = [('2025-01-01', 100),
+           ('2025-05-02', 85),
+           ('2025-02-03', 10),
+            ]
+eventos_dict = dict(eventos)
+print(eventos_dict)
+
+# IMPORTANTE: Si hay dos tuplas con la misma clave
+# en la lista de tuplas, solo se conservara la ultima tupla.
+# Es decir, el valor de la clave correspondiente sera el valor
+# de la utlima tupla en la lista.
+
+# DE LISTAS A DICCIONARIOS
+# zip(): función built-in que devuelve un iterador de tuplas
+# Cada tupla contiene elementos en la misma posición de varios iterables
+
+# Ejemplo general
+keys = ['key1', 'key2', 'key3']
+values = ['value1', 'value2', 'value3']
+
+# zip une las dos listas en pares (clave, valor)
+# dict convierte esas tuplas en un diccionario
+mi_dict = dict(zip(keys, values))
+print("Diccionario general:", mi_dict)
+
+# Ejemplo práctico
+materias = ['mate', 'history', 'filosofia']
+notas = [8.5, 7.0, 10]
+
+# Une materias con sus notas correspondientes
+notas_dict = dict(zip(materias, notas))
+print("Diccionario de notas:", notas_dict)
+
+# Resultado esperado:
+# Diccionario general: {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
+# Diccionario de notas: {'mate': 8.5, 'history': 7.0, 'filosofia': 10}
+# La funcion ZIP() combina las dos listas
+# materias y notas en una lista de tuplas
+# de elementos correspondientes, y leugo el
+# constructor de diccionarios dict() crea un
+# diccionario a partir de esta lista de tuplas.
+
+# Importante: AMBAS LISTAS DEBEN TENER LA MISMA LONGITUD
+# EN OTRO CASO SOLO COPIARA HASTA LA LOGITUD DE LA LISTA MAS CORTA
+
+
+# De SET A DICCIONARIO
+# Sintaxis
+my_set = {('key1', value1), ('key2', value2), ('key3', value3)}
+mi_dictt = dict(my_set)
+
+# Ej:
+
+notas_set = {('mate', 7.0), ('historia', 3.0), ('lengua', 9.0)}
+notas_dict = dict(notas_set)
+print(notas_dict)
