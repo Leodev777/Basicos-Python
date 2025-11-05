a3877b233aa49a4b06cdf1f25e49a79e61554550
Leonardogit <leonardoiglesias74@gmail.com>
2025-11-05 10:50:44 -0300

    T6 C4

--- Tema 6 Diccionarios/Clase 4 Práctica 6b Diccionarios (Parte 1)/Codigo registro_asistencias.py
+++ Tema 6 Diccionarios/Clase 4 Práctica 6b Diccionarios (Parte 1)/Codigo registro_asistencias.py
@@ -2,13 +2,13 @@
 asistencias = dict()
 
 # Registrar asistencias de estudiantes
-asistencias["Estudiante1"] = ["2022-01-01", "2022-01-03", "2022-01-05"]
-asistencias["Estudiante2"] = ["2022-01-02", "2022-01-05", "2022-01-07"]
-asistencias["Estudiante3"] = ["2022-01-01", "2022-01-07", "2022-01-09"]
+asistencias["Estudiante1"] = ["2022-01-01", "2025-10-03", "2025-09-05"]
+asistencias["Estudiante2"] = ["2022-01-02", "2025-10-05", "2025-09-07"]
+asistencias["Estudiante3"] = ["2022-01-01", "2025-10-07", "2025-09-09"]
 
 # Agregar nuevas fechas
-asistencias["Estudiante1"].append("2022-01-07")
-asistencias["Estudiante2"].append("2022-01-09")
+asistencias["Estudiante1"].append("2025-01-07")
+asistencias["Estudiante2"].append("2025-01-09")
 
 # Mostrar la lista de estudiantes y las fechas en las que asistieron
 print("Registro de Asitencias:")
